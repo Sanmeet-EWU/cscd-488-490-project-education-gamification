39a400917d0500f9d45cd2b960b71a5c05a14450
justinmitchell341 <justin.mitchell341@gmail.com>
2024-11-04 16:00:39 -0800

    Firebase setup

---
+++ Project/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDRzsA9k8nv5MfsBXFUCSXzGmNHp8rXTvM",
+  authDomain: "education-gamification.firebaseapp.com",
+  projectId: "education-gamification",
+  storageBucket: "education-gamification.firebasestorage.app",
+  messagingSenderId: "481951042794",
+  appId: "1:481951042794:web:42696d9447acf377771b93"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
