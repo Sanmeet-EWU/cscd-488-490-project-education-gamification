89cc038e6522e06f7b24f9b82240a41d7da5e3f4
justinmitchell341 <justin.mitchell341@gmail.com>
2024-12-02 14:55:02 -0800

    Clean Firebase Slate

---
+++ Project2/firebase/firebase.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDRzsA9k8nv5MfsBXFUCSXzGmNHp8rXTvM",
+    authDomain: "education-gamification.firebaseapp.com",
+    projectId: "education-gamification",
+    storageBucket: "education-gamification.firebasestorage.app",
+    messagingSenderId: "481951042794",
+    appId: "1:481951042794:web:42696d9447acf377771b93"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export async function fetchData() {
+    try {
+        const docRef = doc(db, "Inventorys", "PlayerID-Inventory");
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+            console.log("Document data:", docSnap.data());
+        } else {
+            console.log("No such document!");
+        }
+    } catch (error) {
+        console.error("Error getting document:", error);
+    }
+}
---
+++ Project2/src/main.js
@@ -0,0 +1,35 @@
+import { Boot } from './scenes/Boot';
+import { Game } from './scenes/Game';
+import { GameOver } from './scenes/GameOver';
+import { MainMenu } from './scenes/MainMenu';
+import { Preloader } from './scenes/Preloader';
+import { Leaderboard } from './scenes/Leaderboard';
+import { Settings } from './scenes/Settings';
+import firebase from 'firebase/app';
+import 'firebase/firestore'; 
+import 'firebase/auth'; 
+
+//  Find out more information about the Game Config at:
+//  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
+const config = {
+    type: Phaser.AUTO,
+    width: 1024,
+    height: 768,
+    parent: 'game-container',
+    backgroundColor: '#028af8',
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    scene: [
+        Boot,
+        Preloader,
+        MainMenu,
+        Game,
+        GameOver,
+        Leaderboard,
+        Settings
+    ]
+};
+
+export default new Phaser.Game(config);
