1da2b3ad96d2b66ccc088eec1be85ae10e33b6c8
Trevor Stewart <tstewart12@ewu.edu>
2025-01-27 00:27:42 -0800

    audio can be changed in game menu

--- Project2/src/scenes/Act1Scene1.js
+++ Project2/src/scenes/Act1Scene1.js
@@ -15,7 +15,7 @@ export class Act1Scene1 extends GameScene
     create ()
     {
         this.text = this.add.text(100, 100, 'Act 1 Scene 1, This is place holder', { fontSize: 40 });
-        
+
         this.initializeOptions();
     }
 
--- Project2/src/scenes/Preloader.js
+++ Project2/src/scenes/Preloader.js
@@ -50,6 +50,7 @@ export class Preloader extends Scene
         this.load.image('closeMenuButton', 'closeMenuButton.png');
         this.load.image('settingsMenuButton', 'settingsMenuButton.png');
         this.load.image('toMainMenuButton', 'toMainMenuButton.png');
+        this.load.image('saveGameButton', 'saveGameButton.png');
     }
 
     create ()
