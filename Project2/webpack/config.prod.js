fcf7dfc8a9351a39b2bf31d45ad5b6ec933d31ef
justinmitchell341 <justin.mitchell341@gmail.com>
2025-02-10 13:17:46 -0800

    Test for favicon

--- Project2/webpack/config.prod.js
+++ Project2/webpack/config.prod.js
@@ -69,7 +69,9 @@ module.exports = {
                 { from: "public/style.css", to: "style.css" }, // Copy CSS separately
                 { from: "public/site.webmanifest", to: "site.webmanifest" }, // Ensure webmanifest is included
                 { from: "game.html", to: "game.html" },
+                {from: "public/favicon", to: "favicon"},
                 { from: "public/assets/fonts", to: "font" } // Ensure fonts are copied properly
+
             ]
         })        
     ]
